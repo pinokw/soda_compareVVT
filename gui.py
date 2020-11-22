5bdc658ccbdee002bf759e1bb2a9e1a9203271ef
PIN <andreas@apjsoft.de>
2020-11-22 15:03:08 +0100

    Minor changes

--- gui.py
+++ gui.py
@@ -10,7 +10,7 @@ fileDir = os.path.dirname(os.path.realpath('__file__'))
 sys.path.append(fileDir)
 
 root=Tk.Tk()
-root.geometry('650x300')
+root.geometry('800x300')
 root.configure(bg='#2bab8d')
 fr_input=Tk.LabelFrame(root, text="Eingabe der VVT Vorlage", padx=5, pady=5)
 fr_input.grid(column=0, row=0, padx=10, pady=10)
@@ -38,8 +38,10 @@ def check_vvt():
     ToDo_return=TODO_check.main(xls_old, xls_new)
     str_datei=entry_name.get()
     write_xls.print_xls(vvt_return, tom_return, ToDo_return, str_datei)
-    print('Das Workbook mit den Ergebnissen wurde erstellt')
-
+    if len(str_datei) > 0 :
+        Tk.messagebox.showinfo(title='Success', message=f"Die Datei {str_datei} wurde erstellt")
+    else: 
+        Tk.messagebox.showinfo(title='Success', message=f"Die Datei wurde erstellt")
 
 cmd_input=Tk.Button(fr_input, text="VVT ALT",width=20, bg='#7dcae8' , command=lambda: xls_input(1))
 cmd_input.grid(column=0, row=0)
@@ -49,10 +51,12 @@ cmd_input=Tk.Button(fr_input, text="Neue VVT Version (xls)", bg='#2bab47', width
 cmd_input.grid(column=2, row=0)
 cmd_check=Tk.Button(fr_input, text="Überprüfung der VVTs", command=check_vvt)
 cmd_check.grid(column=1, row=1)
-label_entry=Tk.Label(fr_input, text="Dateiname XLS Output")
+label_entry=Tk.Label(fr_input, text="")
 label_entry.grid(column=0, row=2)
-entry_name=Tk.Entry(fr_input, text="Dateiname")
-entry_name.grid(column=1, row=2)
+label_entry=Tk.Label(fr_input, text="Dateiname XLS Output")
+label_entry.grid(column=0, row=3)
+entry_name=Tk.Entry(fr_input, text="Dateiname", width=40)
+entry_name.grid(column=1, row=3, columnspan=2)
 
 
 # Ausgabe
---
+++ start.bat
@@ -0,0 +1,3 @@
+@ECHO OFF
+python C:\Users\ap-datenschutz\Documents\Code\soda_compareVVT\gui.py
+PAUSE
